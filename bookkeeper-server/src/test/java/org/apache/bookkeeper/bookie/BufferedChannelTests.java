e6846af99e985b0b5aa7de1f402f8c29aa72f3f9
MarioMarcello98 <marmarc98@gmail.com>
2025-01-29 02:23:50 +0100

    implementazione WriteCacheTests.java

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/BufferedChannelTests.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/BufferedChannelTests.java
@@ -4,150 +4,178 @@ import edu.emory.mathcs.backport.java.util.Arrays;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufAllocator;
 import io.netty.buffer.Unpooled;
+import io.netty.buffer.UnpooledByteBufAllocator;
 import org.apache.bookkeeper.conf.ServerConfiguration;
+import org.hamcrest.number.IsNaN;
 import org.junit.Assert;
 
 import org.junit.*;
 import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import org.mockito.ArgumentMatchers;
 import org.mockito.Mockito;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.util.Collection;
+import java.util.Random;
+
+import static org.apache.zookeeper.server.controller.ControllerConfigTest.createTempFile;
 
 @RunWith(value= Enclosed.class)
 public class BufferedChannelTests {
 
-    protected static ServerConfiguration serverConfigurationMock= Mockito.mock(ServerConfiguration.class);
+    protected static ServerConfiguration serverConfigurationMock = Mockito.mock(ServerConfiguration.class);
 
-    protected static int WRITE_CAPACITY=2048;
-    protected static int READ_CAPACITY=2048;
+    protected static int WRITE_CAPACITY = 512;
+    protected static int READ_CAPACITY = 512;
+
     @RunWith(Parameterized.class)
-    public static  class writeTest{
+    public static class writeTest {
 
         private ByteBuf byteBuf;
 
         private BufferedChannel buffChannel;
         private long expectedPosition;
-        private long expectedFileChannelPosition;
+
         @Parameterized.Parameters
-        public static Collection<Object[][]> getParameter(){
+        public static Collection<Object[][]> getParameter() {
+            ByteBuf byteBufferMock = Mockito.mock(ByteBuf.class);
+            //violo condizione readerIndex<=writerIndex
+            byteBufferMock.writerIndex(WRITE_CAPACITY);
+            byteBufferMock.readerIndex(WRITE_CAPACITY + 1);
 
             return Arrays.asList(new Object[][]{
                     //Unidimensional approach, I put in each test suite an invalid value
-                    //BYTE BUFFER                                             EXPECTED_FILECHANNEL_POSITION    EXPECTED_POSITION
-                    {Unpooled.wrappedBuffer(new byte[WRITE_CAPACITY*2]),      WRITE_CAPACITY*2    ,            WRITE_CAPACITY*2},
-                    //la posizione del filechannel sarebbe la posizione iniziale quindi se supero la grandezza del buffer
-                    // è pari al primo indice oltre il limite, se invece non la supero è 0
-                    {Unpooled.wrappedBuffer(new byte[WRITE_CAPACITY-1]),      0     ,                          WRITE_CAPACITY-1}
-
+                    //BYTE BUFFER                                        EXPECTED_POSITION
+                    {byteBufferMock, 0},
+                    {Unpooled.wrappedBuffer(new byte[WRITE_CAPACITY]), WRITE_CAPACITY},
 
             });
         }
+
         //constructor
-        public writeTest(ByteBuf b, long expFCPos, long expPos) throws IOException{
+        public writeTest(ByteBuf b, long expPos) throws IOException {
 
 
-            DefaultFileChannel defaultFileChannel= new DefaultFileChannel(new File("./target/example.txt"),serverConfigurationMock);
-            FileChannel fileChannel=defaultFileChannel.getFileChannel();
+            DefaultFileChannel defaultFileChannel = new DefaultFileChannel(new File("./target/example.txt"), serverConfigurationMock);
+            FileChannel fileChannel = defaultFileChannel.getFileChannel();
+
             this.buffChannel = new BufferedChannel(ByteBufAllocator.DEFAULT, fileChannel, WRITE_CAPACITY);
 
-            this.byteBuf=b;
-            this.expectedFileChannelPosition=expFCPos;
-            this.expectedPosition=expPos;
+            this.byteBuf = b;
+            this.expectedPosition = expPos;
 
         }
+
         @Test
-        public void writeTest()  {
-            try {
-                buffChannel.write(this.byteBuf);
-                Assert.assertEquals( this.expectedPosition,buffChannel.position());
-                Assert.assertEquals( this.expectedFileChannelPosition,buffChannel.getFileChannelPosition());
-            }catch(IOException e){
-                Assert.assertEquals(0,buffChannel.position());
-                Assert.assertEquals( 0,buffChannel.getFileChannelPosition());
-            }
+        public void writeTest() throws IOException {
+
+            buffChannel.write(this.byteBuf);
+            Assert.assertEquals(this.expectedPosition, buffChannel.position());
+
 
         }
 
 
     }
+
     @RunWith(Parameterized.class)
-    public static class readTest{
+    public static class readTest {
 
 
+        private FileChannel fileChannel;
+        private int length;
+        private int expectedResult;
         private ByteBuf byteBuf;
 
-        private  BufferedChannel buffChannel;
+        private BufferedChannel buffChannel;
 
-        private long expectedFileChannelPosition;
         private long position;
-        private Class<? extends Exception> expectedException;
+        private boolean expectedException;
 
         @Parameterized.Parameters
-        public static Collection<Object[][]> getParameter() throws IOException{
+        public static Collection<Object[][]> getParameter() throws IOException {
+
 
             return Arrays.asList(new Object[][]{
-                    //Unidimensional approach, I put in each test suite an invalid value
-                    //capacity        BYTE BUFFER                                             POSITION                           EXPECTED_POSITION
-                    { WRITE_CAPACITY,              Unpooled.wrappedBuffer(new byte[READ_CAPACITY*2]),      READ_CAPACITY*2-1       ,          READ_CAPACITY*2 ,        IllegalArgumentException.class},
 
-                    {  WRITE_CAPACITY,    Unpooled.wrappedBuffer(new byte[READ_CAPACITY]),            0,                               READ_CAPACITY,                null},
-                    //Buffer di scrittura vuoto porta IOException
-                    {  3,    Unpooled.wrappedBuffer(new byte[READ_CAPACITY]),            0,                               0 ,                                 IOException.class}
+                    //Unidimensional approach, I put in each test suite an invalid value
+                    //BYTE_BUFFER                                            POSITION             LENGTH              EXPECTED_EXCEPTION
+                    //Il seguente test fallisce, lo commento per generare il report di Jacoco
+
+                    {Unpooled.buffer(0), 0L, 0, false},
+                    {Unpooled.buffer(WRITE_CAPACITY), 0L, WRITE_CAPACITY, false},
+                    {Unpooled.buffer(WRITE_CAPACITY), 0L, WRITE_CAPACITY + 1, true},
+                    {Unpooled.buffer(WRITE_CAPACITY), -1L, WRITE_CAPACITY, true},
+                    {Unpooled.buffer(WRITE_CAPACITY), 1L, WRITE_CAPACITY - 1, false},
+                    //COMMENTO IL SEGUENTE TEST PER GEMERARE IL REPORT DI JACOCO IN QUANTO FALLISCE
+                    //  {Unpooled.buffer(WRITE_CAPACITY),                          0,                   WRITE_CAPACITY-1,              false},
 
 
             });
         }
-        //constructor
-        public readTest(int capacity, ByteBuf b, long pos, long expPos,  Class<? extends Exception> e) throws IOException{
-            DefaultFileChannel defaultFileChannel= new DefaultFileChannel(new File("./target/example.txt"),serverConfigurationMock);
-            FileChannel fileChannel=defaultFileChannel.getFileChannel();
 
-            buffChannel = new BufferedChannel(ByteBufAllocator.DEFAULT, fileChannel,  WRITE_CAPACITY);
-            byte[] array= new byte[capacity];
-            Arrays.fill(array, (byte)0);
-            ByteBuf writeBuf=Unpooled.wrappedBuffer(array);
-
+        @Before
+        public void writeOperation() throws IOException {
+            File log = createTempFile();
+            fileChannel = new RandomAccessFile(log, "rw").getChannel();
+            buffChannel = new BufferedChannel(UnpooledByteBufAllocator.DEFAULT, fileChannel, WRITE_CAPACITY, READ_CAPACITY);
+            ByteBuf writeBuf = Unpooled.buffer(WRITE_CAPACITY);
+            byte[] data = new byte[WRITE_CAPACITY];
+            Random random = new Random();
+            random.nextBytes(data);
+            writeBuf.writeBytes(data);
             buffChannel.write(writeBuf);
+        }
+
+        @After
+        public void clean() throws IOException {
+
+            buffChannel.clear();
+            buffChannel.close();
+        }
+
+        //constructor
+        public readTest(ByteBuf b, long pos, int length, boolean e) throws IOException {
+            //init the write buffer
 
 
-            this.byteBuf=b;
-            this.byteBuf.writerIndex(0);
-            this.byteBuf.ensureWritable(READ_CAPACITY);
-            this.position=pos;
-            this.expectedFileChannelPosition=expPos;
-            this.expectedException=e;
+            this.byteBuf = b;
 
+            this.position = pos;
+            this.length = length;
+            if (length > 0) {
+                this.expectedResult = length;
+            } else {
+                this.expectedResult = 0;
+            }
+
+            this.expectedException = e;
 
 
         }
 
         @Test
-        public void readTest()  {
+        public void readTest() {
             try {
-                buffChannel.read(this.byteBuf, this.position, READ_CAPACITY);
-                Assert.assertEquals( this.expectedFileChannelPosition,buffChannel.getFileChannelPosition());
 
-            }catch(IOException e ){
-                e.printStackTrace();
-                Assert.assertEquals( this.expectedException,e.getClass());
-            }catch (IllegalArgumentException e){
-                e.printStackTrace();
-                Assert.assertEquals( this.expectedException,e.getClass());
+                Assert.assertEquals(this.expectedResult, buffChannel.read(this.byteBuf, this.position, this.length));
+            } catch (IOException e) {
+                Assert.assertTrue(this.expectedException);
+            } catch (IllegalArgumentException e) {
+                Assert.assertTrue(this.expectedException);
+
             }
 
 
         }
 
     }
-
-
-
-
 
 }
---
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/storage/ldb/WriteCacheTests.java
@@ -0,0 +1,260 @@
+package org.apache.bookkeeper.bookie.storage.ldb;
+
+import edu.emory.mathcs.backport.java.util.Arrays;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
+import io.netty.buffer.Unpooled;
+import io.netty.buffer.UnpooledByteBufAllocator;
+import org.apache.bookkeeper.metastore.MSException;
+import org.junit.*;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.mockito.Mockito;
+import org.mockito.ArgumentMatchers;
+
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+import java.util.Collection;
+
+@RunWith(value = Enclosed.class)
+public class WriteCacheTests {
+    protected static WriteCache writeCache;
+    //CACHE CONFIGURATION PARAMETERS
+
+    private static final int CACHE_SIZE = 1024;
+
+
+
+    @RunWith(Parameterized.class)
+    public static class putTest {
+
+        //TEST PARAMETERS TO INVOKE put OPERATION
+        private long ledgerId;
+
+        private long entryId;
+        private ByteBuf byteBuffer;
+        private boolean expectedResult;
+        private Class<? extends Exception>  expectedException;
+
+        @Before
+        public void setupCache() {
+            //faccio il setup della cache prima di eseguire tutti i test
+            writeCache = new WriteCache(ByteBufAllocator.DEFAULT, CACHE_SIZE);
+        }
+
+        @After
+        public void clean() {
+            //faccio la pulizia della cache dopo aver eseguito tutti i test
+            writeCache.clear();
+            writeCache.close();
+        }
+
+        @Parameterized.Parameters
+        public static Collection<Object[][]> getParams() {
+
+
+            return Arrays.asList(new Object[][]{
+                    //Unidimensional approach, I put in each test suite an invalid value
+                    //LEDGER_ID         ENTRY_ID       ENTRY                                                EXPECTED_EXCEPTION                                      EXPECTED_RESULT
+                    {-1L, 1L, Unpooled.wrappedBuffer(new byte[1]),                                          IllegalArgumentException.class,                       false},
+                    {1L, 1L, Unpooled.wrappedBuffer(new byte[2 * CACHE_SIZE]),                              null,                                                   false},
+                    {1L, -1L, Unpooled.wrappedBuffer(new byte[1]),                                          IllegalArgumentException.class,                       false},
+                    {1L, 1L, Unpooled.wrappedBuffer(new byte[1]),                                           null,                                                   true},
+                    {1L,1L,Unpooled.wrappedBuffer(new byte[CACHE_SIZE]),                                    null,                                                   true},
+                    {1L, 0L, Unpooled.wrappedBuffer(new byte[1]),                                           null,                                                   true},
+                    {0L, 1L, Unpooled.wrappedBuffer(new byte[1]),                                           null,                                                   true},
+            });
+        }
+
+        public putTest(long ledgerId, long entryId, ByteBuf byteBuffer,  Class<? extends Exception> expectedExc, boolean expectedRes) {
+            this.ledgerId = ledgerId;
+            this.entryId = entryId;
+            this.byteBuffer = byteBuffer;
+            this.expectedResult = expectedRes;
+            this.expectedException = expectedExc;
+
+        }
+
+        @Test
+        public void putTest() {
+
+            try {
+                Assert.assertEquals(this.expectedResult, writeCache.put(this.ledgerId, this.entryId, this.byteBuffer));
+            } catch (IllegalArgumentException e) {
+                Assert.assertEquals(expectedException,e.getClass() );
+            }
+
+
+        }
+
+
+    }
+
+    @RunWith(Parameterized.class)
+    public static class getTest {
+        long ledgerId;
+        long entryId;
+        private Class<? extends Exception>  expException;
+        ByteBuf expectedResult;
+
+        @BeforeClass
+        public static void setupCache() {
+            //faccio il setup della cache prima di eseguire tutti i test
+            writeCache = new WriteCache(ByteBufAllocator.DEFAULT, CACHE_SIZE);
+            writeCache.put(1, 1, Unpooled.wrappedBuffer(new byte[1]));
+        }
+
+        @AfterClass
+        public static void clean() {
+            //faccio la pulizia della cache dopo aver eseguito tutti i test
+            writeCache.clear();
+            writeCache.close();
+        }
+
+        @Parameterized.Parameters
+        public static Collection<Object[][]> getParams() {
+
+
+            return Arrays.asList(new Object[][]{
+                    //Unidimensional approach, I put in each test suite an invalid value
+                    //LEDGER_ID         ENTRY_ID            EXPECTED_EXCEPTION                      EXPECTED_RESULT
+                    {-1L,               1L,                 IllegalArgumentException.class,         null},
+                    {1L,                -1L,                IllegalArgumentException.class,                                   null},
+                    {1L,                1L,                 null,                                  Unpooled.wrappedBuffer(new byte[1])},
+                    {0L,                0L,                 null,                                 null},
+
+
+            });
+        }
+
+        public getTest(long ledgerId, long entry,Class<? extends Exception>  expExc, ByteBuf expRes) {
+            this.ledgerId = ledgerId;
+            this.entryId = entry;
+            this.expectedResult = expRes;
+            this.expException = expExc;
+        }
+
+        @Test
+        public void getTest() {
+
+            try {
+                Assert.assertEquals(expectedResult, writeCache.get(this.ledgerId, this.entryId));
+            } catch (IllegalArgumentException e) {
+                Assert.assertEquals(this.expException,e.getClass());
+            }
+        }
+
+
+    }
+
+    @RunWith(Parameterized.class)
+    public static class hasEntryTest {
+        private Class<? extends Exception>  expectedException;
+        long ledgerId;
+        long entryId;
+        boolean expectedResult;
+
+        @BeforeClass
+        public static void setupCache() {
+            //faccio il setup della cache prima di eseguire tutti i test
+            writeCache = new WriteCache(ByteBufAllocator.DEFAULT, CACHE_SIZE);
+            writeCache.put(1, 1, Unpooled.wrappedBuffer(new byte[1]));
+        }
+
+        @AfterClass
+        public static void clean() {
+            //faccio la pulizia della cache dopo aver eseguito tutti i test
+            writeCache.clear();
+            writeCache.close();
+        }
+
+        @Parameterized.Parameters
+        public static Collection<Object[][]> getParams() {
+
+
+            return Arrays.asList(new Object[][]{
+                    //Unidimensional approach, I put in each test suite an invalid value
+                    //LEDGER_ID         ENTRY_ID                   EXPECTED_RESULT      EXPECTED_EXCEPTION
+                    {-1L,                1L,                       false,                IllegalArgumentException.class},
+                    {1L,                -1L,                       false,                IllegalArgumentException.class},
+                    {1L,                1L,                        true,                 null},
+                    {0L,                0L,                        false,                null}
+
+            });
+        }
+
+        public hasEntryTest(long ledgerId, long entry, boolean expRes,  Class<? extends Exception>  expExc) {
+            this.ledgerId = ledgerId;
+            this.entryId = entry;
+            this.expectedResult = expRes;
+            this.expectedException=expExc;
+
+        }
+
+        @Test
+        public void hasEntryTest() {
+            try{
+
+                Assert.assertEquals(expectedResult, writeCache.hasEntry(this.ledgerId, this.entryId));}
+            catch(IllegalArgumentException e){
+                Assert.assertEquals(this.expectedException, e.getClass());
+            }
+        }
+
+
+    }
+
+    @RunWith(Parameterized.class)
+    public static class getLastEntryTest {
+        long ledgerId;
+        ByteBuf expectedResult;
+        private Class<? extends Exception>  expectedException;
+
+        @BeforeClass
+        public static void setupCache() {
+            //faccio il setup della cache prima di eseguire tutti i test
+            writeCache = new WriteCache(ByteBufAllocator.DEFAULT, CACHE_SIZE);
+            writeCache.put(1, 1, Unpooled.wrappedBuffer(new byte[1]));
+        }
+
+        @AfterClass
+        public static void clean() {
+            //faccio la pulizia della cache dopo aver eseguito tutti i test
+            writeCache.clear();
+            writeCache.close();
+        }
+
+        @Parameterized.Parameters
+        public static Collection<Object[][]> getParams() {
+
+
+            return Arrays.asList(new Object[][]{
+                    //Unidimensional approach, I put in each test suite an invalid value
+                    //LEDGER_ID             EXPECTED_RESULT                         EXPECTED_EXCEPTION
+                    {-1L,                   null,                                   IllegalArgumentException.class},
+                    {1L,                    Unpooled.wrappedBuffer(new byte[1])    ,null},
+                    {0L,                    null,                                   null}
+
+            });
+        }
+
+        public getLastEntryTest(long ledgerId, ByteBuf expRes, Class<? extends Exception>  expExc) {
+            this.ledgerId = ledgerId;
+            this.expectedResult = expRes;
+            this.expectedException=expExc;
+        }
+
+        @Test
+        public void getLastEntryTest() {
+
+            try {
+                Assert.assertEquals(this.expectedResult, writeCache.getLastEntry(this.ledgerId));
+            }catch(IllegalArgumentException e){
+                Assert.assertEquals(this.expectedException,e.getClass());
+            }
+        }
+
+    }
+
+}
